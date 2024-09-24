6084ffd5b39dfab3a9ded9f39c578e89ed6c6878
Stéphane - Solution Logique <stephane.pt@solution-logique.local>
2024-09-24 17:30:47 +0200

    Premier push, init du projet

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RecipeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/recettes', name: 'recipe.index')]
+    public function index(Request $request, RecipeRepository $recipeRepository): Response
+    {
+        //On veut les recettes dont la durée est inf ou égale à...
+        //La méthode est dans le repository
+        $recipes = $recipeRepository->findWithDurationLowerThan(40);
+
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes
+        ]);
+    }
+    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function show(Request $request, string $slug, int $id, RecipeRepository $recipeRepository): Response
+    {
+        $recipe = $recipeRepository->find($id);
+        if ($recipe->getSlug() != $slug) {
+            return $this->redirectToRoute('recipe.show', ['slug' => $recipe->getSlug(), 'id' => $recipe->getId()]);
+        }
+        return $this->render('recipe/show.html.twig', [
+            'recipe' => $recipe
+        ]);
+    }
+}
---
+++ src/Repository/RecipeRepository.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Recipe;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Recipe>
+ */
+class RecipeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Recipe::class);
+    }
+
+    /**
+     * 
+     * @return Recipe[]
+     */
+    public function findWithDurationLowerThan(int $duration): array
+    {
+        //Si on veut communiquer avec la base de manière profonde, on utilise le QueryBuilder
+        return $this->createQueryBuilder('r')
+            ->where('r.duration <= :duration')
+            ->orderBy('r.duration', 'ASC')
+            ->setMaxResults(10)
+            ->setParameter('duration', $duration)
+            ->getQuery()
+            ->getResult();
+    }
+
+    //    /**
+//     * @return Recipe[] Returns an array of Recipe objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('r.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+    //    public function findOneBySomeField($value): ?Recipe
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}

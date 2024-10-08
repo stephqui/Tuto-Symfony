1e42b15f3a286f02e84c809e26f41b2b69bf0499
stephqui <paradivuiz@gmail.com>
2024-10-08 23:05:40 +0200

    Form : fin chapitre : postSubmit

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -44,7 +44,6 @@ class RecipeController extends AbstractController
     $form = $this->createForm(RecipeType::class, $recipe);
     $form->handleRequest($request);
     if ($form->isSubmitted() && $form->isValid()) {
-      $recipe->setUpdatedAt(new \DateTimeImmutable());
       $em->flush();
       $this->addFlash('success', 'La recette a bien été modifiée');
       return $this->redirectToRoute('recipe.index');
@@ -62,8 +61,6 @@ class RecipeController extends AbstractController
     $form = $this->createForm(RecipeType::class, $recipe);
     $form->handleRequest($request);
     if ($form->isSubmitted() && $form->isValid()) {
-      $recipe->setCreatedAt(new \DateTimeImmutable());
-      $recipe->setUpdatedAt(new \DateTimeImmutable());
       $em->persist($recipe);
       $em->flush();
       $this->addFlash('success', 'La recette a bien été créée');
--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Recipe;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PostSubmitEvent;
 use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -18,20 +20,16 @@ class RecipeType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('slug')
+            ->add('slug', TextType::class, [
+              'required'=> false
+            ])
             ->add('content')
             ->add('duration')
             ->add('save', SubmitType::class, [
                 'label' => 'Envoyer'
             ])
             ->addEventListener(FormEvents::PRE_SUBMIT, $this->autoSlug(...))
-            ->get('createdAt')->addEventListener(
-                FormEvents::POST_SUBMIT,
-                function (FormEvent $event) {
-                    
-                }
-
-            )
+            ->addEventListener(FormEvents::POST_SUBMIT, $this->attachTimeStamps(...))
         ;
     }
     public function autoSlug(PreSubmitEvent $event):void
@@ -45,6 +43,17 @@ class RecipeType extends AbstractType
         }
     }
 
+    public function attachTimeStamps(PostSubmitEvent $event){
+      //On veut remplir les champs de date selon les besoins
+      $data = $event->getData();
+      if(!($data instanceof Recipe)){
+        return;
+      }
+      $data->setUpdatedAt(new \DateTimeImmutable());
+      if(!($data->getId())){
+        $data->setCreatedAt(new \DateTimeImmutable());
+      }
+    }
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([

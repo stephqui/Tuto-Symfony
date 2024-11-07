62a23a0cfb1a42abb089ccca7ce25125339c9739
Stéphane - Solution Logique <stephane.pt@solution-logique.local>
2024-11-07 17:29:42 +0100

    Relation Many to one : début

---
+++ migrations/Version20241107155226.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241107155226 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX id ON category');
+        $this->addSql('DROP INDEX Idx ON category');
+        $this->addSql('DROP INDEX Idx_2 ON category');
+        $this->addSql('DROP INDEX name ON category');
+        $this->addSql('ALTER TABLE category CHANGE Id id INT AUTO_INCREMENT NOT NULL, CHANGE slug slug VARCHAR(60) NOT NULL, CHANGE created_at created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated_at updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE recipe ADD category_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE recipe ADD CONSTRAINT FK_DA88B13712469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_DA88B13712469DE2 ON recipe (category_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category CHANGE id Id TINYINT(1) NOT NULL, CHANGE slug slug VARCHAR(100) NOT NULL, CHANGE created_at created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, CHANGE updated_at updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX id ON category (name)');
+        $this->addSql('CREATE INDEX Idx ON category (Id)');
+        $this->addSql('CREATE INDEX Idx_2 ON category (Id)');
+        $this->addSql('CREATE UNIQUE INDEX name ON category (name)');
+        $this->addSql('ALTER TABLE recipe DROP FOREIGN KEY FK_DA88B13712469DE2');
+        $this->addSql('DROP INDEX IDX_DA88B13712469DE2 ON recipe');
+        $this->addSql('ALTER TABLE recipe DROP category_id');
+    }
+}

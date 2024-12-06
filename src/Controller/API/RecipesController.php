1dda7906696d9ca88fad19a9b75a867847da12a3
Stéphane - Solution Logique <stephane.pt@solution-logique.local>
2024-12-06 15:53:56 +0100

    APi serializer 10'30

---
+++ src/Controller/API/RecipesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\API;
+
+use App\Repository\RecipeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipesController extends AbstractController
+{
+
+    #[Route("/api/recipes")]
+    public function index(RecipeRepository $recipeRepository){
+       $recipes = $recipeRepository->findAll();
+       return $this->json($recipes);
+    }
+}

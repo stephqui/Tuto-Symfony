dcbe40558a1da2d0b18947be00e40ad47699d95f
Stéphane - Solution Logique <stephane.pt@solution-logique.local>
2024-12-12 17:29:00 +0100

    API: début deserializer 2'50

--- src/Controller/API/RecipesController.php
+++ src/Controller/API/RecipesController.php
@@ -13,15 +13,22 @@ use Symfony\Component\Serializer\SerializerInterface;
 class RecipesController extends AbstractController
 {
 
-   #[Route("/api/recipes")]
+   #[Route("/api/recipes", methods:["GET"])]
    public function index(RecipeRepository $recipeRepository, Request $request, SerializerInterface $serializer)
    {
       $recipes = $recipeRepository->paginateRecipes($request->query->getInt('page', 1));
  
       //Quand on crée une API, et qu'on veut renvoyer des données, on a cette méthode
       //json si on veut faire du Json, mais on peut créer des méthodes pour d'autres format.
+
+      //Exemple ci-dessous en 'csv'
+      /*dd($serializer->serialize($recipes, 'csv', [
+         'groups'=>['recipes.index']
+      ]));*/
       //On peut utiliser les groupes de serialization pour annoter notre entité pour expliquer
       //quand est-ce qu'on dit renvoyer qqchose. Ca à l'avantage d'utiliser moins de ressources.
+
+      //Là, on utilise abstracController, qui a la méthode 'json' intégrée
       return $this->json($recipes, 200, [], [
          'groups' => ['recipes.index']
       ]);
@@ -35,5 +42,12 @@ class RecipesController extends AbstractController
       return $this->json($recipe, 200, [], [
          'groups' => ['recipes.index', 'recipes.show']
       ]);
+   }
+
+   #[Route("/api/recipes",methods:["POST"])]
+   public function create(Request $request, SerializerInterface $serializer)
+   {
+      //dd($request->getContent());
+      dd($serializer->deserialize($request->getContent(), Recipe::class, 'json'));
    }
 }

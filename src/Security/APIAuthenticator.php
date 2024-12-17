570df9b866287f1d1cd6b3dcbd0b074a4fdf6a11
Stéphane - Solution Logique <stephane.pt@solution-logique.local>
2024-12-17 15:21:13 +0100

    Stateless: pb connexion utilisateur avec clé API 2.0

--- src/Security/APIAuthenticator.php
+++ src/Security/APIAuthenticator.php
@@ -2,22 +2,25 @@
 
 namespace App\Security;
 
+use SebastianBergmann\Environment\Console;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 
 class APIAuthenticator extends AbstractAuthenticator
 {
 
     public function supports(Request $request): ?bool
-    {;
-        dd($request->headers);
+    {
+        ;
+        //dd($request->headers);
+        //return $request->headers->has('Authorization') && str_contains($request->getRequestUri(), "/api/");
         return $request->headers->has('Authorization') &&
             str_contains($request->headers->get('Authorization'), 'Bearer ');
     }
@@ -25,7 +28,7 @@ class APIAuthenticator extends AbstractAuthenticator
     public function authenticate(Request $request): Passport
     {
         $identifier = str_replace('Bearer ', '', $request->headers->get('Authorization'));
-        //dd($identifier);
+        //  dd($identifier);
         return new SelfValidatingPassport(
             new UserBadge($identifier)
         );
